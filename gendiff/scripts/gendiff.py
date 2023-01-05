3403577d5048b0e7c5452534a3e8082cfab77cc1
Pavel Bychenko <pavelv87@gmail.com>
2023-01-05 15:02:22 +0600

    finish step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,15 +4,15 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    import argparse
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    # print(args.first_file)
+    # print(args.second_file)
 
 
 if __name__ == '__main__':
