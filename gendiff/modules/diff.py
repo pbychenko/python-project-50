e3c89cf322cc6e6e6fc7d8203618e3603e7eb19c
Pavel Bychenko <pavelv87@gmail.com>
2023-01-19 21:00:26 +0600

    fix changes

--- gendiff/modules/diff.py
+++ gendiff/modules/diff.py
@@ -45,30 +45,32 @@ from .formatter import format_data
 
 def generate_diff(file_path1, file_path2):
     [file1_data, file2_data] = parse_files(file_path1, file_path2)
+    print(file2_data)
     ast = get_ast(file1_data, file2_data)
     print('ast', ast)
     return ast
 
-def get_ast(data1, data2):
-    
+def get_ast(data1, data2):    
     set1 = set(data1.keys())
     set2 = set(data2.keys())
-    # minus = set1 - set2
-    # plus = set2 - set1
-    # per = set1 & set2
 
-    all_keys = set1 | set2
-    # print(all_keys)
+    all_keys = list(set1 | set2)
+    all_keys.sort()
+    # print('all_keys', all_keys)
     
     def get_ast_element(key):
-        if (data1.get(key) and data1[key] is dict) and (data2.get(key) and data2[key] is dict):
-            print('key', key)
+        # print(data1)
+        # print('key', key)
+        # print('key in data1', key in data1)
+        # print(data1.get(key))
+        # print('data1[key] is dict', data1[key] is dict)
+        if (key in data1 and isinstance(data1[key], dict)) and (key in data2 and isinstance(data2[key], dict)):
             return { 'key': key, 'state': 'nested', 'children': get_ast(data1[key], data2[key]) }
-        if data1.get(key) and data2.get(key):
+        if key in data1 and key in data2:
             if (data1[key] == data2[key]):
                 return { 'key':key, 'state': 'equal', 'value': data1[key] }
             return { 'key': key, 'state': 'changed', 'value': data1[key], 'new_value': data2[key] }
-        if data1.get(key):
+        if key in data1:
             return { 'key': key, 'state': 'removed', 'value': data1[key] }
         return { 'key': key, 'state': 'added', 'value': data2[key] }
 
