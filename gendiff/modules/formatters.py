c18fdf5427de9a04dbe2b2107c285a66adfd0fda
pavelv <pavelv87@gmail.com>
2023-01-21 10:44:55 +0600

    fix changes

--- gendiff/modules/formatters.py
+++ gendiff/modules/formatters.py
@@ -15,31 +15,44 @@ import os
 
 def formatElement(el, indent_count):
     indents = ' '*indent_count
-    if isinstance(el, dict):
-        return '{\n' + f'{indents}{el}' + f"\n{indents}" + '}'
-    return el
+    if not isinstance(el, dict):
+        return el
     
-def stylish(data):
-    indent_count = 2
+    # print('el', el)
+    result = []
+
+    for k, v in el.items():
+        result.append(f'{indents}{k}: {v}')
+    result_str = '\n'.join(result)
+    print(indent_count)
+    print('result_str', result_str)
+
+
+    return '{\n' + f'{result_str}' + f"\n{' '*(indent_count - 4)}" + '}'
+    
+def stylish(data, indent_count = 2 ):
     indents = ' '*indent_count
     
     def getElement(el):
         if (el['state'] == 'nested'):
-            return f"{indents}  {el['key']}: {formatElement(stylish(el['children']), indent_count + 2)}"
+            # return f"{indents}  {el['key']}: {formatElement(stylish(el['children']), indent_count + 2)}"
+            return f"{indents}  {el['key']}: {stylish(el['children'], indent_count + 4)}"
+        
         if (el['state'] == 'added'):
-            return f"{indents}+ {el['key']}: {formatElement(el['value'], indent_count + 2)}"
+            return f"{indents}+ {el['key']}: {formatElement(el['value'], indent_count + 4)}"
         if (el['state'] == 'removed'):
-            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 2)}"
+            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 4)}"
         if (el['state'] == 'equal'):
-            return f"{indents}  {el['key']}: {formatElement(el['value'], indent_count + 2)}"
+            return f"{indents}  {el['key']}: {formatElement(el['value'], indent_count + 4)}"
         if (el['state'] == 'changed'):
-            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 2)}'\n'{indents}+ {el['key']}: {formatElement(el['new_value'], indent_count + 2)}"
+            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 4)}\n{indents}+ {el['key']}: {formatElement(el['new_value'], indent_count + 4)}"
     
     # print(list(map(getElement, data)))
-    result = '{\n' + f"{indents}" +'\n'.join(list(map(getElement, data))) + '\n}'    
-    # result = f"{' '*indent_count}" +'\n'.join(list(map(getElement, data)))
-
-    # result = json.dumps(data, indent = 2).replace('"', '').replace(',', '')
-    # print(result)
+    # result = '{\n' + f"{indents}" +'\n'.join(list(map(getElement, data))) + '\n}'
+    # t1 = list(map(getElement, data))
+    # print(t1)
+    # flatten = [item for sublist in t1 for item in sublist]
+    result = '{\n' + '\n'.join(list(map(getElement, data))) + f"\n{' '*(indent_count - 2)}" + '}'
+    # result = '{\n' + '\n'.join(flatten) + f"\n{' '*(indent_count - 2)}" + '}'
     return result
     
