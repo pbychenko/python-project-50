35579267c0210b535e5d29f4490ce46cb270d17e
Pavel Bychenko <pavelv87@gmail.com>
2023-01-23 20:25:59 +0600

    fix cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 from gendiff.modules.diff import generate_diff
+import os
 
 
 def main():
@@ -13,10 +14,18 @@ def main():
     parser.add_argument("-f", "--format", help="set format of output")
 
     args = parser.parse_args()
+
     print(args.first_file)
     print(args.second_file)
+    p1 = os.path.join(os.getcwd(), args.first_file)
+    p2 = os.path.join(os.getcwd(), args.second_file)
     print(args.format)
-    # print(generate_diff(args.first_file, args.second_file, format))
+    print(os.getcwd())
+    # print(generate_diff(p1, p2, args.format))
+    if args.format:
+        print(generate_diff(args.first_file, args.second_file, args.format))
+    else:
+        print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
--- tests/test_json_diff.py
+++ tests/test_json_diff.py
@@ -39,6 +39,7 @@ def test_big_json_plain():
 
 def test_big_json_json():
     print(os.getcwd())
+
     file1_path = os.path.join(os.getcwd(), 'tests', 'fixtures', 'json', 'file11.json')
     file2_path = os.path.join(os.getcwd(), 'tests', 'fixtures', 'json', 'file21.json')
     result_path = os.path.join(os.getcwd(), 'tests', 'fixtures', 'results', 'result_json.txt')
