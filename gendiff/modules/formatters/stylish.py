d8b516c7e0e487a4b562d64bd5abe50f073a7123
pavelv <pavelv87@gmail.com>
2023-02-05 11:53:53 +0600

    fix changes

--- gendiff/modules/formatters/stylish.py
+++ gendiff/modules/formatters/stylish.py
@@ -1,4 +1,55 @@
-def formatElement(el, indent_count):
+# def formatElement(el, indent_count):
+#     if isinstance(el, bool):
+#         return str(el).lower()
+
+#     if el is None:
+#         return 'null'
+
+#     if not isinstance(el, dict):
+#         return el
+
+#     result = []
+#     indents = ' ' * indent_count
+
+#     for k, v in el.items():
+#         result.append(f'  {indents}{k}: {formatElement(v, indent_count + 4)}')
+#     result_str = '\n'.join(result)
+
+#     return '{\n' + f'{result_str}' + f"\n{' '*(indent_count - 2)}" + '}'
+
+
+# def stylish(data, indent_count=2):
+#     indents = ' ' * indent_count    
+
+#     def get_element(el):
+#         if (el['state'] == 'nested'):
+#             return f"{indents}  {el['key']}: {stylish(el['children'], indent_count + 4)}"
+        
+#         value = formatElement(el['value'], indent_count + 4)
+
+#         if (el['state'] == 'added'):
+#             return f"{indents}+ {el['key']}: {value}"
+
+#         if (el['state'] == 'removed'):
+#             return f"{indents}- {el['key']}: {value}"
+
+#         if (el['state'] == 'equal'):
+#             return f"{indents}  {el['key']}: {value}"
+
+#         if (el['state'] == 'changed'):
+#             return f"{indents}- {el['key']}: {value}\n{indents}+ {el['key']}: {formatElement(el['new_value'], indent_count + 4)}"        
+
+
+    
+#     result = '{\n' + '\n'.join(list(map(get_element, data))) + f"\n{' '*(indent_count - 2)}" + '}'
+
+#     return result
+
+indent_step = 4
+indent_template = '  '
+
+
+def formatElement(el, depth):
     if isinstance(el, bool):
         return str(el).lower()
 
@@ -9,30 +60,41 @@ def formatElement(el, indent_count):
         return el
 
     result = []
-    indents = ' ' * indent_count
+    indents = indent_template * (depth + indent_step + 1)
+    closing_indents = indent_template * (depth + 1)
 
     for k, v in el.items():
-        result.append(f'  {indents}{k}: {formatElement(v, indent_count + 4)}')
+        result.append(f'{indents}{k}: {formatElement(v, depth + indent_step)}')
     result_str = '\n'.join(result)
+    # print(' result_str', result_str)
 
-    return '{\n' + f'{result_str}' + f"\n{' '*(indent_count - 2)}" + '}'
+    return '{\n' + f'{result_str}' + f"\n{closing_indents}" + '}'
 
 
-def stylish(data, indent_count=2):
-    indents = ' ' * indent_count
+def stylish(data, depth=1):
+    indents = indent_template * depth
+
 
     def get_element(el):
         if (el['state'] == 'nested'):
-            return f"{indents}  {el['key']}: {stylish(el['children'], indent_count + 4)}"
+            return f"{indents}  {el['key']}: {stylish(el['children'], depth + indent_step)}"
+        
+        value = formatElement(el['value'], depth)
 
         if (el['state'] == 'added'):
-            return f"{indents}+ {el['key']}: {formatElement(el['value'], indent_count + 4)}"
-        if (el['state'] == 'removed'):
-            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 4)}"
-        if (el['state'] == 'equal'):
-            return f"{indents}  {el['key']}: {formatElement(el['value'], indent_count + 4)}"
-        if (el['state'] == 'changed'):
-            return f"{indents}- {el['key']}: {formatElement(el['value'], indent_count + 4)}\n{indents}+ {el['key']}: {formatElement(el['new_value'], indent_count + 4)}"
+            return f"{indents}+ {el['key']}: {value}"
 
-    result = '{\n' + '\n'.join(list(map(get_element, data))) + f"\n{' '*(indent_count - 2)}" + '}'
+        if (el['state'] == 'removed'):
+            return f"{indents}- {el['key']}: {value}"
+
+        if (el['state'] == 'equal'):
+            return f"{indents}  {el['key']}: {value}"
+
+        if (el['state'] == 'changed'):
+            return f"{indents}- {el['key']}: {value}\n{indents}+ {el['key']}: {formatElement(el['new_value'], depth)}"    
+
+
+    
+    result = '{\n' + '\n'.join(list(map(get_element, data))) + f"\n{indent_template * (depth - 1)}" + '}'
+
     return result
